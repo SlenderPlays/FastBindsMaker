a2ff8e1bf418069dfdca088d6970c129a4c8833f
SlenderPlays <georgemagic10@gmail.com>
2019-03-25 22:44:44 +0200

    Update 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,9 @@ $(document).ready(function(){
 		$(".select-three").val("none").change();
 		$(".output").val("");
 		$(".bind-name").val("");
+		$(".bind-adder-adding-number-checkbox").prop('checked', false).trigger("change");
+		$(".bind-stopper-public-two").prop('checked', false).trigger("change");
+		$(".bind-stopper-public-three").prop('checked', false).trigger("change");
 	});
 	$(".select-one").change(function(){
 		if($(".select-one").val() == "text")
@@ -23,11 +26,37 @@ $(document).ready(function(){
 		if($(".select-one").val() == "dropdown")
 		{
 			$(".select-one-dropdown").css('display', 'block');
-			
 		}
 		else
 		{
 			$(".select-one-dropdown").css('display', 'none');
+			var optionsToDelete = $(".bind-dropdown-option-delete-one").length - 1;
+			$(".bind-dropdown-option-delete-one").each(function () {
+				if(optionsToDelete != 0)
+				{
+					$(this).click();
+				}
+				optionsToDelete = optionsToDelete - 1;
+			});
+			$(".bind-dropdown-option-one").val("");
+		}
+
+		if($(".select-one").val() == "adder")
+		{
+			$(".select-one-adder").css('display', 'block');
+		}
+		else
+		{
+			$(".select-one-adder").css('display', 'none');
+			$(".bind-adder-comment").val("");
+			$(".bind-adder-starting-number").val("");
+			$(".bind-adder-ending-number").val("");
+			$(".bind-adder-digits").val("0");
+			$(".bind-adder-adding-number").val("");
+			$(".bind-adder-adding-number-min").val("");
+			$(".bind-adder-adding-number-max").val("");
+			$(".bind-adder-delay").val("");
+			$(".bind-adder-timer-name").val("");
 		}
 	});
 	$(".select-two").change(function(){
@@ -49,6 +78,28 @@ $(document).ready(function(){
 		else
 		{
 			$(".select-two-dropdown").css('display', 'none');
+			var optionsToDelete = $(".bind-dropdown-option-delete-two").length - 1;
+			$(".bind-dropdown-option-delete-two").each(function () {
+				if(optionsToDelete != 0)
+				{
+					$(this).click();
+				}
+				optionsToDelete = optionsToDelete - 1;
+			});
+			$(".bind-dropdown-option-two").val("");
+		}
+
+		if($(".select-two").val() == "stopper")
+		{
+			$(".select-two-stopper").css('display', 'block');
+			
+		}
+		else
+		{
+			$(".select-two-stopper").css('display', 'none');
+
+			$(".bind-stopper-timer-name-two").val("");
+			$(".bind-stopper-message-two").val("");
 		}
 		
 	});
@@ -71,8 +122,28 @@ $(document).ready(function(){
 		else
 		{
 			$(".select-three-dropdown").css('display', 'none');
+			var optionsToDelete = $(".bind-dropdown-option-delete-three").length - 1;
+			$(".bind-dropdown-option-delete-three").each(function () {
+				if(optionsToDelete != 0)
+				{
+					$(this).click();
+				}
+				optionsToDelete = optionsToDelete - 1;
+			});
+			$(".bind-dropdown-option-three").val("");
 		}
-		
+		if($(".select-three").val() == "stopper")
+		{
+			$(".select-three-stopper").css('display', 'block');
+			
+		}
+		else
+		{
+			$(".select-three-stopper").css('display', 'none');
+
+			$(".bind-stopper-timer-name-three").val("");
+			$(".bind-stopper-message-three").val("");
+		}
 	});
 	$(".bind-dropdown-addOption-one").click(function(){
 		$(this).parent().find("span").last().clone().insertAfter($(this).parent().find("span").last());
@@ -107,6 +178,55 @@ $(document).ready(function(){
 			}	
 		});
 	});
+	$(".bind-adder-adding-number-checkbox").change(function(){
+		if(this.checked)
+		{
+			// Random
+			$(".bind-adder-adding-number-container").css('display', 'none');
+			$(".bind-adder-adding-number-random-container").css('display', 'block');
+
+			$(".bind-adder-adding-number").val("");
+		}
+		else
+		{
+			// Not Random
+			$(".bind-adder-adding-number-random-container").css('display', 'none');
+			$(".bind-adder-adding-number-container").css('display', 'block');
+
+			$(".bind-adder-adding-number-min").val("");
+			$(".bind-adder-adding-number-max").val("");
+		}
+	});
+	$(".bind-stopper-public-two").change(function(){
+		console.log(this.checked);
+		
+
+		if(this.checked)
+		{
+			// Public
+			$(".bind-stopper-message-two-container").css('display', 'block');
+		}
+		else
+		{
+			$(".bind-stopper-message-two-container").css('display', 'none');
+			$(".bind-stopper-message-two").val("");
+		}
+	});
+	$(".bind-stopper-public-three").change(function(){
+		console.log(this.checked);
+		
+
+		if(this.checked)
+		{
+			// Public
+			$(".bind-stopper-message-three-container").css('display', 'block');
+		}
+		else
+		{
+			$(".bind-stopper-message-three-container").css('display', 'none');
+			$(".bind-stopper-message-three").val("");
+		}
+	});
 	$(".submit-button").click(function(){
 		var outputText = "";
 		outputText += $(".bind-name").val();
@@ -125,6 +245,28 @@ $(document).ready(function(){
 				outputText += "||" + $(this).val();
 			});
 		}
+		if($(".select-one").val() == "adder")
+		{
+			outputText += "||ADDER||";
+			outputText += $(".bind-adder-comment").val() + "||";
+			outputText += $(".bind-adder-starting-number").val() + "||";
+			outputText += $(".bind-adder-ending-number").val() + "||";
+			outputText += $(".bind-adder-digits").val() + "||";
+			if(!$(".bind-adder-adding-number-checkbox").is(":checked"))
+			{
+				outputText += $(".bind-adder-adding-number").val() + "||";
+			}
+			else
+			{
+				outputText += "RAND=";
+				outputText += $(".bind-adder-adding-number-min").val();
+				outputText += "="
+				outputText += $(".bind-adder-adding-number-max").val();
+				outputText += "||"
+			}
+			outputText += $(".bind-adder-delay").val() + "||";
+			outputText += $(".bind-adder-timer-name").val();
+		}
 
 		outputText += "|//";
 
@@ -139,6 +281,21 @@ $(document).ready(function(){
 				outputText += "||" + $(this).val();
 			});
 		}
+		if($(".select-two").val() == "stopper")
+		{
+			outputText += "||STOPPER||";
+			outputText += $(".bind-stopper-timer-name-two").val();
+			outputText += "||";
+			if(!$(".bind-stopper-public-two").is(":checked"))
+			{
+				outputText += "false||None";				
+			}
+			else
+			{
+				outputText += "true||";
+				outputText += $(".bind-stopper-message-two").val();
+			}
+		}
 
 		outputText += "|//";
 
@@ -152,6 +309,21 @@ $(document).ready(function(){
 			$(".bind-dropdown-option-three").each(function () {
 				outputText += "||" + $(this).val();
 			});
+		}
+		if($(".select-three").val() == "stopper")
+		{
+			outputText += "||STOPPER||";
+			outputText += $(".bind-stopper-timer-name-three").val();
+			outputText += "||";
+			if(!$(".bind-stopper-public-three").is(":checked"))
+			{
+				outputText += "false||None";				
+			}
+			else
+			{
+				outputText += "true||";
+				outputText += $(".bind-stopper-message-three").val();
+			}
 		}
 
 		
